68f7bb52f449a3619efce22f5530f5edf8ae99ad
RobinRother <robin.rother@web.de>
2022-02-05 14:01:06 +0100

    add rasa files

---
+++ actions/actions.py
@@ -0,0 +1,37 @@
+
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import AllSlotsReset
+ 
+ 
+class ActionReadSelectedOptionDescription(Action):
+    """
+    handler for the option selection action
+    """
+   
+    def name(self) -> Text:
+        return "read_selected_option_decription"
+  
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+ 
+        """
+        selects the option to describe depending on user choice
+        """
+
+        selected_option = tracker.get_slot("option")
+
+        if selected_option == "one":
+            dispatcher.utter_message(response="utter_option_one")
+        elif selected_option == "two":
+            dispatcher.utter_message(response="utter_option_two")
+        elif selected_option == "three":
+            dispatcher.utter_message(response="utter_option_three")
+        elif selected_option == "four":
+            dispatcher.utter_message(response="utter_option_four")
+        else:
+            dispatcher.utter_message(response="utter_option_invalid", option=selected_option)
+ 
+        return [AllSlotsReset()]
